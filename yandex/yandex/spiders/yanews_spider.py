f57cfcf8fb50b9c1da2e5224a7d4a25cb1bbfc9a
icemanovich <icemanovich@gmail.com>
2016-04-08 16:33:33 +0300

    Add YandexSubject and YandexDonor parsers

--- yandex/yandex/spiders/yanews_spider.py
+++ yandex/yandex/spiders/yanews_spider.py
@@ -2,7 +2,9 @@
 from __future__ import absolute_import
 import scrapy
 from scrapy import http
+from scrapy.utils.response import open_in_browser
 import time
+import hashlib
 from bs4 import BeautifulSoup
 from .. import items
 
@@ -38,23 +40,43 @@ class YanewsSpider(scrapy.Spider):
         self.logger.warning('Craws url :: {0}'.format(response.url))
         self.save_to_file(response.body)
 
-        soup = BeautifulSoup(response.body, 'html.parser')
-        content = soup.find("div", attrs={"class": "page-content__left"})
+        # open_in_browser(response)
+        """ ------------------------------ """
 
-        blocks = content.find_all('li', attrs={"class": "search-item"})
+        soup = BeautifulSoup(response.body, 'html.parser')
+        blocks = soup.find("div", attrs={"class": "page-content__left"}).find_all('li', attrs={"class": "search-item"})
 
         for item in blocks:
             d = ''
             # div = item.find('div', attrs={"class": "document i-bem"})
-            if item.has_attr('id'):
+            li = item.next
+            if 'story-item' in li.get('class'):
                 # this is subject
                 # -> request and parse for SUBJECT!!!!
-                pass
+                try:
+
+                    subject_link = li.find('h2', attrs={'class': 'story-item__title'}).next['href']
+                    yield http.Request(self.format_subject_url(subject_link), callback=self.parse_subjects)
+                except Exception as e:
+                    self.logger.error(e)
+
             else:
                 # this is donor
-                #cretae Donor object and save it
-                pass
+                # create Donor object and save it
+                donor = items.YandexDonor()
+                try:
+                    title_tag = item.find('div', attrs={'class': 'document__title'}).next
+                    donor['link'] = title_tag['href']
+                    donor['title'] = title_tag.get_text()
+                    donor['description'] = item.find('div', attrs={'class': 'document__snippet'}).get_text()
 
+                    ''' TODO :: Figure out how to parse correct data '''
+                    donor['published_at'] = item.find('div', attrs={'class': 'document__time'}).get_text()
+                    donor['created_at'] = str(int(time.time()))
+
+                    yield donor
+                except Exception as e:
+                    self.logger.error(e)
 
         g = ''
 
@@ -63,51 +85,68 @@ class YanewsSpider(scrapy.Spider):
     #   request next page
     #     if self.is_next_page_exists(content.get_text()):
     #         self.current_page += 1
-    #         yield http.Request('', callback=self.parse_subject)  # crawl SUBJECT and DONORS
+    #         yield http.Request('', callback=self.parse)  # crawl SUBJECT and DONORS
 
         # yield .....SOMETHING FRONT PAGE DONORS
 
-
-    def is_next_page_exists(self, html):
-
-        if '<span class="button__text">Следующая</span>' in html:
-            return True
-
-        return False
-
-
-
-
-
-
-
-
-        # yield Someth
-
-
-    #     parse page
-    #     get subjects - > request it
-    #     get donors -> request it
-
-
     def parse_subjects(self, response):
-        pass
+        """
+        Parse response HTML and create Donor object
 
-    def parse_donors(self, response):
-        pass
+        :param response: HtmlResponse
+        :return:
+        """
+        a = ''
 
-    def extract_subject_link(self):
-        pass
+        self.save_to_file(response.body, 'subject')
+        open_in_browser(response)
 
-    def extract_donor_link(self):
-        pass
+        soup = BeautifulSoup(response.body, 'html.parser')
 
+        subject = items.YandexSubject()
+        title = soup.find('h1', attrs={'class': 'story__head'})
+        subject['title'] = title.get_text()
 
+        #  TODO:: Does not work with cyrillic characters !!
+        # subject['title_hash'] = hashlib.md5(subject['title'])
+
+        subject['link'] = response.url
+        subject['created_at'] = str(int(time.time()))
+        subject['donors_count'] = 0
+
+        try:
+            content = soup.find('div', attrs={'class': 'story__main'})
+            for item in content.find_all('div', attrs={'class': 'story__group'}):
+                donor = items.YandexDonor()
+
+                d_title = item.find('h2', attrs={'class': 'doc__title'})
+
+                donor['title'] = d_title.next.get_text()
+                donor['link'] = d_title.next['href']
+                donor['description'] = item.find('div', attrs={'class': 'doc__content'}).next.get_text()
+                donor['published_at'] = item.find('div', attrs={'class': 'doc__time'}).get_text()
+
+                donor['subject_id'] = hashlib.md5(subject['link']).hexdigest()
+
+                subject['donors_count'] += 1
+                yield donor
+
+                # TODO :: Try to yield both objects !!!
+                # yield donor, subject
+
+        except Exception as e:
+            self.logger.error('Error in parse_subjects method on getting donor. {0}'.format(e))
 
     @staticmethod
-    def format_url(search_string='', page=0):
-        """
+    def is_next_page_exists(html):
+        if '<span class="button__text">Следующая</span>' in html:
+            return True
+        return False
 
+    @staticmethod
+    def format_url(search_string='', page=0, only_today=False):
+        """
+        :param only_today: bool
         :param search_string: str
         :param page: str
         :return: str
@@ -115,11 +154,21 @@ class YanewsSpider(scrapy.Spider):
         # if ' ' in search_string:
         #     search_string = '+'.join(search_string.split(' '))
 
-        return 'https://news.yandex.ru/yandsearch?rpt=nnews2&grhow=clutop&text={0}&rpt=nnews2&rel=rel&numdoc=30&p={1}&noreask=1'.format(search_string, page)
+        period = ''
+        if only_today:
+            period = '&within=7'
+
+        return 'https://news.yandex.ru/yandsearch?rpt=nnews2&grhow=clutop&text={0}&rpt=nnews2&rel=rel&numdoc=30&p={1}&noreask=1{2}'.format(search_string, page, period)
+
+    @staticmethod
+    def format_subject_url(donor_link):
+        return 'https://news.yandex.ru/{0}&content=alldocs'.format(donor_link.strip('/'))
+
+    """ ================================ """
 
     # DEBUG ONLY
     @staticmethod
-    def save_to_file(string):
-        text_file = open("Output.html", "w")
+    def save_to_file(string, filename='Output'):
+        text_file = open("{0}.html".format(filename), "w")
         text_file.write(string)
         text_file.close()
